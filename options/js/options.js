5dd2ddadc1adf7dbeb1129aeb0bc0a588cb7489b
Barack Karavani <barakyo@baracks-mbp.lan>
2019-08-04 15:24:37 -0700

    Options styling and naming conventions

--- options/js/options.js
+++ options/js/options.js
@@ -1,7 +1,7 @@
 function onSubmit(event) {
-  const patNode = document.getElementById("personalAccessToken");
+  const patNode = document.getElementById("personal-access-token");
   chrome.storage.sync.set({ personalAccessToken: patNode.value });
-  const optionsForm = document.getElementById("optionsForm");
+  const optionsForm = document.getElementById("options-form");
   console.log(optionsForm.classList);
   optionsForm.className = "hide";
   const saved = document.getElementById("saved");
@@ -11,3 +11,10 @@ function onSubmit(event) {
 
 const saveButton = document.getElementById("save-btn");
 saveButton.onclick = onSubmit;
+
+chrome.storage.sync.get(["personalAccessToken"], result => {
+  if (result.personalAccessToken) {
+    const patNode = document.getElementById("personal-access-token");
+    patNode.value = result.personalAccessToken;
+  }
+});
